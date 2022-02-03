b4985b13a8b5ff0d78607acebfb2ab5fab4d2096
nishant111 <nshntsharma86@gmail.com>
2022-02-03 17:04:12 +0530

    Time: 91 ms (20.75%), Space: 12.5 MB (42.58%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.c
@@ -0,0 +1,16 @@
+
+void swap(char *arr, int left, int right)
+{
+    arr[left] = arr[left] + arr[right];
+    arr[right] = arr[left] - arr[right];
+    arr[left] = arr[left] - arr[right];
+}
+
+void reverseString(char* s, int sSize){
+    int left = 0;
+    int right = sSize-1;
+    for(;left<right;left++,right--)
+    {
+        swap(s, left, right);
+    }
+}
