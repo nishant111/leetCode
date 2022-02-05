a956bfb6294d8999e7f3b138a6245e1b70b9461f
nishant111 <nshntsharma86@gmail.com>
2022-02-05 11:41:50 +0530

    Time: 10 ms (50.98%), Space: 6.9 MB (50.33%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,25 @@
+//keep repeating string a in each iteration and keep finding b each time
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        int count = 0;
+        string temp="";
+        while(temp.length()<=a.length() + b.length() )
+        {
+            if(temp.find(b) < temp.length())
+            {
+               return count;
+            }
+            else
+            {   
+                temp= temp.append(a);
+                //cout <<"Didnt find, repeated string is now "<<temp<<"\n";
+                count++;
+            }
+        }
+        if(temp.find(b) < temp.length())
+            return count;
+        else
+            return -1;
+    }
+};
