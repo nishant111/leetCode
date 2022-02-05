ba6b05c1ee3fd2cb75fe27a484725d8a06842b85
nishant111 <nshntsharma86@gmail.com>
2022-02-05 18:14:11 +0530

    Time: 21 ms (20.41%), Space: 7.1 MB (94.66%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map <char,int> map;
+        for(int i = 0;i<s.length();i++)
+        {
+            if(map.find(s[i])!=map.end())
+                map[s[i]]+=1;
+            else
+                map[s[i]]=1;
+        }
+        for(int i = 0;i<t.length();i++)
+        {
+            if( map.find(t[i]) != map.end() )
+            {
+                map[t[i]]-=1;
+                if(map[t[i]] == 0)
+                    map.erase(t[i]);
+            }
+            else 
+                return false;
+                
+        }
+        if(map.size()==0) return true;
+        else return false;
+    }
+};
