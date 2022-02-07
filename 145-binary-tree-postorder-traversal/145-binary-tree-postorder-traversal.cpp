54780902bdc22146f26c7e12fcd80f85c5fd3d90
nishant111 <nshntsharma86@gmail.com>
2022-02-07 11:09:00 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (64.80%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -21,7 +21,66 @@ public:
     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        dfs(root, ans);
+        if(root == NULL) return ans;
+        //dfs(root, ans);
+        /* iterative soln using two stack, can also be done in one stack */
+        stack <TreeNode *> st;
+        stack <TreeNode *> ch;
+        TreeNode *curr = root;
+        ch.push(root);
+        while(ch.empty()!=true)
+        {
+            TreeNode *tmp = ch.top();
+            st.push(tmp);
+            ch.pop();
+            if(tmp->left)
+                ch.push(tmp->left);
+            if(tmp->right)
+                ch.push(tmp->right);
+        }
+        
+        while(st.empty()!=true)
+        {
+            ans.push_back(st.top()->val);
+            st.pop();
+        }
+        
         return ans;
+        
+        
+        
+        /*while(curr || st.empty()!=true)
+        {
+            if(curr)
+            {
+                st.push(curr);
+                curr = curr ->left;
+            }
+            else
+            {
+                //curr = st.top();
+                //st.pop();
+                if(st.top()->right == NULL)
+                {
+                    ans.push_back();
+                    node *tmp = st.top();
+                    st.pop;
+                    curr = st.top();
+                    if(curr->right == tmp)
+                    {
+                        
+                    }
+                }
+
+                else
+                {
+                    //curr->right exists need to go there and traceback and print root
+                    curr = st.top()->right;
+
+                }
+                //st.pop();
+            }
+        }
+        return ans;*/
     }
 };
