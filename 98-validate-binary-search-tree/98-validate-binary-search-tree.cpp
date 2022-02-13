77d90b1621cb91ceca188491097928e7d7cd66a6
nishant111 <nshntsharma86@gmail.com>
2022-02-13 13:37:44 +0530

    Time: 14 ms (62.48%), Space: 21.7 MB (32.32%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // sub means subtree
+    bool isSubtreeLesser(TreeNode *sub, int targ)
+    {
+        if(sub == NULL) return true;
+        if (sub->val >= targ ) return false;
+        return (isSubtreeLesser(sub->left, targ) &&
+                isSubtreeLesser(sub->right, targ) );
+        //return true;
+    }
+    //sub means subtree
+    bool isSubtreeGreater(TreeNode *sub, int targ)
+    {
+        if(sub == NULL) return true;
+        if(sub->val <= targ ) return false;
+        return ( isSubtreeGreater(sub->left, targ) &&
+                 isSubtreeGreater(sub->right, targ) );
+          //return true;
+    }
+    bool dfs(TreeNode *root)
+    {
+        if(root == NULL) return true;
+        return (isSubtreeLesser(root->left, root->val) && 
+                isSubtreeGreater(root->right, root->val) &&
+                dfs(root->left) &&
+                dfs(root->right));
+    }
+    bool isValidBST(TreeNode* root) {
+        return dfs(root);
+    }
+};
