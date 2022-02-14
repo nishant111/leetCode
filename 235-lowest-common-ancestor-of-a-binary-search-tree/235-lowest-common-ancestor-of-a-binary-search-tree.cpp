f5928427a2b2b7de2dc6c93fc50087e8498ac6cd
nishant111 <nshntsharma86@gmail.com>
2022-02-14 10:47:56 +0530

    Time: 28 ms (90.82%), Space: 23.3 MB (64.54%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,7 +7,35 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/* Soln using BST Property O(log n)*/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode *curr = root;
+        while(curr !=NULL)
+        {
+            if( (p->val <= curr->val && q->val >= curr->val)  ||
+                (p->val >= curr->val && q->val <= curr->val ))
+            {
+                // split happening here, hence this is LCA
+                return curr;
+            }
+            else if(p->val > curr->val && q->val > curr->val)
+            {
+                curr = curr-> right; 
+            }
+            else
+            {
+                curr = curr->left;
+            }
+        }
+        return NULL;
+    }
+};
 
+
+/*
+DFS Solution (O(n))
 class Solution {
 public:
     TreeNode * dfs(TreeNode * root, TreeNode *p, TreeNode *q)
@@ -24,4 +52,4 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
       return (dfs(root, p, q));  
     }
-};
+};*/
