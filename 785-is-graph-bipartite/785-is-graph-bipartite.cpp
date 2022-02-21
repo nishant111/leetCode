6cd222e54c979c628de2cdb4587d79e6544c7266
nishant111 <nshntsharma86@gmail.com>
2022-02-21 13:52:21 +0530

    Time: 24 ms (83.76%), Space: 13.6 MB (41.72%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    bool dfsFindOddCycle(vector<vector<int>>& graph, vector<bool> &visited, 
+                         vector<bool> &dfsvisited, int curr, int cyclen, int parent)
+    {
+        visited[curr] = true;
+        dfsvisited[curr] = true;
+        for(auto it:graph[curr])
+        {
+            if(!visited[it])
+            {
+                cout <<"Traversing "<<it <<"\n";
+                if(dfsFindOddCycle(graph, visited, dfsvisited ,it, cyclen+1, curr))
+                    return true;
+            }
+            else if(it == parent)
+            {
+                continue;
+            }
+            else if(visited[it] && dfsvisited[it] )
+            {
+                int cycle = cyclen+1;
+                cout <<"at "<<it<<"\n";
+                cout <<"cycle length is "<<cycle<<"\n";
+                if ((cycle)%2 != 0)
+                    return true;
+            }
+        }
+        cout<<"returning from "<<curr<<"\n";
+        dfsvisited[curr]= false;
+        return false;
+        
+    }
+    
+    bool bfsColoring(vector<vector<int>>& graph, vector<bool> &visited, 
+                    vector<int> &color, int curr)
+    {
+        //using colors 0 and 1
+        queue<int> q;
+        q.push(curr);
+        color[curr] = 0;
+        visited[curr] = true;
+        while(!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            for(auto it: graph[curr])
+            {
+                if(color[it]==-1)
+                {
+                    //unvisited
+                    color[it] = (color[curr]==0?1:0);
+                    q.push(it);
+                }
+                else
+                {
+                    if(color[it]==0 && color[curr]==0)
+                        return false;
+                    else if (color[it]==1 && color[curr] == 1)
+                        return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<bool> visited(graph.size(), false);
+        vector<bool> dfsvisited(graph.size(), false);
+        vector<int> color(graph.size(), -1);
+        // graph is actually a adjency list
+        for(int i = 0;i<graph.size();i++)
+        {
+            
+            //if(!visited[i])
+            if(color[i]==-1)
+            {
+                cout <<"starting point "<<i<<"\n";
+                //if(dfsFindOddCycle(graph, visited, dfsvisited, i, 0, -1))
+                if(!bfsColoring(graph, visited, color, i))
+                    return false; //means coloring not possible
+                //visited[i] = true;
+            }
+        }
+        return true;
+    }
+};
