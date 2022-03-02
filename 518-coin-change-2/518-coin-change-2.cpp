a0026bb9b89e887c272bcc4d67b21d42fd08a353
nishant111 <nshntsharma86@gmail.com>
2022-03-02 10:04:55 +0530

    Time: 36 ms (45.49%), Space: 21.9 MB (7.15%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,5 +1,51 @@
 class Solution {
 public:
+    int dfs(int amount, int curr, vector<int> &coins, vector<vector<int>> &dp )
+    {
+        if(amount < 0 || curr>=coins.size()) return 0;
+        if(amount == 0) return 1;
+        if(dp[amount][curr]!=-1) return dp[amount][curr];
+        int pick = dfs(amount - coins[curr], curr, coins, dp);
+        int nonpick = dfs(amount, curr+1, coins, dp);
+        return dp[amount][curr] = pick+nonpick;
+    }
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp (amount + 1, vector<int>(coins.size(), -1));
+        return dfs(amount, 0, coins, dp);
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+public:
     int dfs(int amount, int curr, vector<int> & coins, int count,
             vector<vector<int>> &dp)
     {
@@ -14,4 +60,4 @@ public:
         vector<vector<int>> dp(amount+1, vector<int>(coins.size(), -1));
         return dfs(amount, 0, coins, 0, dp);
     }
-};
+};*/
