9e32208e16c9087bf362b1b444d3be7c10d2ba3a
nishant111 <nshntsharma86@gmail.com>
2022-03-05 13:44:11 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (24.97%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dfs(int m , int n, int row, int col, vector<vector<int>>& dp)
+    {
+        if(row >= m ||col >= n) return 0; //not valid
+        if(row == m-1 && col == n-1) return 1; //valid path
+        if(dp[row][col]!=-1) return dp[row][col];
+        int down = dfs(m,n, row+1,col, dp);
+        int right = dfs(m,n, row, col+1, dp);
+        return dp[row][col] = down + right;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return dfs(m,n,0,0, dp);
+    }
+};
