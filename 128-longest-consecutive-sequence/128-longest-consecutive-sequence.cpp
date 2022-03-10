066fd120bfc602fa2a1d0c2f8f1e4d487de30862
nishant111 <nshntsharma86@gmail.com>
2022-03-10 10:28:13 +0530

    Time: 760 ms (18.63%), Space: 31.6 MB (25.98%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        vector<bool> vis(nums.size(), 0);
+        unordered_map <int, int> m;
+        int maxcount = 0;
+        for(int i = 0;i<nums.size();i++)
+        {
+            m.insert({nums[i], i});
+        }
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+            int count = 1;
+            if(vis[i]!=0) continue;
+            vis[i] = 1;
+            int targ = nums[i]-1;
+            while(m.find(targ)!=m.end())
+            {
+                count++;
+                vis[m[targ]] = 1;
+                targ--;
+            }
+            maxcount = max(count, maxcount);
+        }
+        return maxcount;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set <int> mySet;
+        int numsSize = nums.size();
+        for(int i = 0;i<numsSize;i++)
+        {
+            mySet.insert(nums[i]);
+        }
+        int longest = 0;
+        for(int i = 0;i<numsSize;i++)
+        {
+            if(!mySet.count(nums[i]-1))
+            {
+                int currentNum = nums[i];
+                int currentStreak = 1;
+                while(mySet.count(currentNum+1))
+                {
+                    currentNum += currentNum;
+                    currentStreak += currentStreak;
+                }
+                if(currentStreak > longest)
+                    longest = currentStreak;
+            }
+        }
+        return longest;
+    }
+};*/
