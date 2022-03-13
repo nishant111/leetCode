d40aead769b44014e87b0b03f05d9cf24d5380e5
nishant111 <nshntsharma86@gmail.com>
2022-03-13 19:14:38 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (91.81%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void swap(vector<int> &nums, int left, int right)
+    {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+    void dfs(vector<int> &nums, vector<vector<int>> &ans, int curr)
+    {
+        if(curr >= nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i = curr;i<nums.size();i++)
+        {
+            swap(nums, i, curr);
+            dfs(nums,  ans, curr+1);
+            swap(nums, i, curr);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> tmp;
+        vector<vector<int>> ans;
+        dfs(nums, ans, 0);
+        return ans;
+    }
+};
