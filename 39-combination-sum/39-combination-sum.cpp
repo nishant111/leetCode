5c3aba5e3445b365e3ffa260f648dcca03c1de28
nishant111 <nshntsharma86@gmail.com>
2022-03-13 10:37:47 +0530

    Time: 8 ms (73.70%), Space: 10.7 MB (96.52%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void dfs(vector<int> &cand, vector<int> &tmp,
+            vector<vector<int>> &ans, int targ, int curr)
+    {
+        if(targ <0 || curr >= cand.size()){
+            return;
+        }
+        else if(targ == 0){
+            ans.push_back(tmp);
+            return;
+        }
+        
+        //pick
+        tmp.push_back(cand[curr]);
+        dfs(cand, tmp, ans, targ-cand[curr], curr);
+        
+        //non-pick
+        tmp.pop_back();
+        dfs(cand, tmp, ans, targ, curr+1);
+        return;
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> tmp;
+        vector<vector<int>> ans;
+        dfs(candidates, tmp, ans ,target, 0);
+        return ans;
+    }
+};
