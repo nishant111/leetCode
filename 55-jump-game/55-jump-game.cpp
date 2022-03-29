08c6e11fcbfe22b2e7a457d04eef7e3f0af47772
nishant111 <nshntsharma86@gmail.com>
2022-03-29 11:14:45 +0530

    Time: 85 ms (64.14%), Space: 50.4 MB (12.74%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool dfs(vector<int> & nums, int curr, vector<int> &dp)
+    {
+        if(curr >= nums.size()) return dp[curr]= false;
+        if(curr == nums.size()-1) return dp[curr] = true;
+        if(dp[curr]!=-1) return dp[curr];
+        bool res;
+        for(int i = nums[curr];i>0;i--)
+        {
+            if(curr+i < nums.size())
+            {
+                if(dfs(nums, curr+i, dp))
+                    return dp[curr] = true;
+            }
+        }
+        return dp[curr] = false;
+    }
+    bool canJump(vector<int>& nums) {
+       vector<int> dp(nums.size()+1, -1);
+       //return dfs(nums, 0, dp);
+        int goal = nums.size()-1;
+        for(int i = nums.size()-2;i>=0;i--)
+        {
+            if(nums[i] + i >= goal)
+                goal = i;
+        }
+        if(goal == 0) return true;
+        return false;
+    }
+};
