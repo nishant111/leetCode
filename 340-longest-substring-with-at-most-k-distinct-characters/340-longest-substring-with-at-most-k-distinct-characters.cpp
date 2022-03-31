a928e4682d27c80d5396a4378aab54563f64801b
nishant111 <nshntsharma86@gmail.com>
2022-03-31 10:34:17 +0530

    Time: 20 ms (51.19%), Space: 8.2 MB (45.44%) - LeetHub

---
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        unordered_map <char, int> m;
+        int ans = 0;
+        int l = 0;
+        int r = 0;
+        while(l<=r && l<s.length() && r<s.length())
+        {
+            if(m.size()==k && m.find(s[r]) == m.end())
+            {
+                //first collect ans as the window is valid for now;
+                ans = max(ans, r - l); //notice r is one ahead
+                
+                //need to release , if We aquire window has k+1 distinct
+                if(m[s[l]]==1)
+                    m.erase(s[l]);
+                else
+                    m[s[l]]-=1;
+                l++;
+            }
+            else
+            {
+                //aquire
+                m[s[r]] += 1;
+                r++;
+            }
+        }
+        ans = max(ans, r-l);
+        return ans;
+    }
+};
