35e125ccd4ec418abe6499970a9fed25ca2ea592
nishant111 <nshntsharma86@gmail.com>
2022-03-31 09:53:03 +0530

    Time: 15 ms, Memory: 7.4 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -7,10 +7,7 @@ public:
         unordered_map <char,int> map;
         for(int i = 0;i<s.length();i++)
         {
-            if(map.find(s[i])!=map.end())
-                map[s[i]]+=1;
-            else
-                map[s[i]]=1;
+            map[s[i]]+=1;
         }
         for(int i = 0;i<t.length();i++)
         {
