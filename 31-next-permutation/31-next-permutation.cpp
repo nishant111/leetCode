8ec67775c75d316b9b2517b4ab2683a87a9d5a5a
nishant111 <nshntsharma86@gmail.com>
2022-04-03 10:10:19 +0530

    Time: 14 ms (7.41%), Space: 12.1 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,23 +1,73 @@
 class Solution {
 public:
-    /*void swap(vector<int> &nums, int left, int right)
+    void reverse1(vector<int> &nums, int indx)
     {
-        int tmp = nums[left];
-        nums[left]=nums[right];
-        nums[right] = tmp;
-    }
-    void reverse(vector<int> &nums, int idx)
-    {
-        int left = idx;
+        int left = indx;
         int right = nums.size()-1;
-        while(left >right)
+        while(left<=right)
         {
-            swap(nums, left, right);
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
             left++;
             right--;
         }
-    }*/
+    }
     void nextPermutation(vector<int>& nums) {
+        
+        int found = true;
+        int indl=0;
+        for(int i = nums.size()-1;i>=1;i--)
+        {
+            if(nums[i-1]<nums[i])
+            {
+                indl = i-1;
+                break;
+            }
+        }
+        int ind2 = 0;
+        for(int i = nums.size()-1;i>indl;i--)
+        {
+            if(nums[i]>nums[indl])
+            {
+                swap(nums[indl], nums[i]);
+                reverse1(nums, indl+1);
+                return;
+            }
+        }
+        sort(nums.begin(), nums.end());
+        return;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    void nextPermutation1(vector<int>& nums) {
         int ind1=-1;
         int ind2=-1;
         for(int i = nums.size()-2;i>=0;i--)
