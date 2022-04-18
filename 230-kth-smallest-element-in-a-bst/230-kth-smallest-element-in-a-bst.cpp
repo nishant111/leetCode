9140097f959f19c2df0b759606fd629918050bff
nishant111 <nshntsharma86@gmail.com>
2022-04-18 10:40:05 +0530

    Time: 18 ms (81.23%), Space: 24.1 MB (60.91%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -9,75 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-/* Do a inorder traversal and keep a count
- * Morris Inorder is the most effecient traversal 
- * As it does not tae extra stack space
- * Hence, we will use that */
 class Solution {
 public:
-    TreeNode* dfs(TreeNode *root, int &count,int k)
+    int dfs(TreeNode *root, int k, int &count)
     {
-        if(root == NULL) return NULL;
-        TreeNode* left = dfs(root->left, count, k);
-        if(left)
-            return left;
-        count++; 
-        if(count == k)
-            return root;
-        TreeNode* right = dfs(root->right, count, k);
-        if(right)
-            return right;
-        return NULL;
+        if(root==NULL) return -1;
+        int left;int right;
+        left = dfs(root->left, k, count);
+        if(left>-1) return left;
+        count++;
+        if(count == k) return root->val;
+        right = dfs(root->right, k, count);
+        if(right>-1) return right;
+        return -1;
     }
     int kthSmallest(TreeNode* root, int k) {
-        int count1 = 0;
-        return dfs(root, count1, k) -> val;
-        TreeNode * curr = root;
-        int ans;
         int count = 0;
-        while(curr !=NULL)
-        {
-            if(curr -> left == NULL)
-            {
-                count ++;
-                cout <<"Processed "<<curr->val <<"\n";
-                if(count == k)
-                {
-                    cout <<"Found result ";
-                    // still need to reset synthetic thread , cant return
-                    ans= curr->val;
-                }
-                curr = curr -> right;
-            }
-            else if(curr -> left!= NULL)
-            {
-                TreeNode *tmp = curr->left;
-                cout <<"tmp is "<<tmp->val<<"\n";
-                while(tmp->right!=NULL && tmp->right != curr)
-                {
-                    tmp = tmp-> right;
-                    cout <<"tmp is "<<tmp->val<<"\n";
-                }
-                if(tmp-> right == NULL)
-                {
-                    tmp->right = curr;
-                    curr= curr->left;
-                }
-                else if(tmp->right == curr)
-                {
-                    // Remove the synthetic thread link to inorder successor
-                    tmp->right = NULL;
-                    // process the curr, as the left subtree is processed
-                    count++;
-                    cout <<"Processed "<<curr->val <<"\n";
-                    if(count == k)
-                        ans = curr->val;
-                    curr= curr-> right;
-                }
-            }
-            
-        }
-        return ans;
+        return dfs(root, k, count);      
     }
 };
