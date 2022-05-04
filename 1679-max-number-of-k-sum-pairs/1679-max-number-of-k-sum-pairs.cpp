b9d1901be387708f5c0fa2c4d74ec381146dc06f
nishant111 <nshntsharma86@gmail.com>
2022-05-04 10:00:14 +0530

    Time: 253 ms (24.46%), Space: 72.9 MB (26.85%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,6 +1,62 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int , int> m;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]] += 1;
+        }
+        
+        for(auto elem:nums)
+        {
+            int target = k - elem;
+            if(m[elem]==0) 
+                continue;
+            m[elem] -= 1;
+            
+            if(m[target]>0)
+            {
+                count++;
+                m[target] -= 1;
+            }
+            else
+            {
+                m[elem] += 1;
+            }
+        }
+        return count;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+O(nlogn) soln
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         
         int l = 0;
@@ -26,4 +82,4 @@ public:
         }
         return count;
     }
-};
+};*/
