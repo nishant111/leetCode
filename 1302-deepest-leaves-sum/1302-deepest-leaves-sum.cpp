7679641ada7da5eb1d95e48000b1fde49b41d899
nishant111 <nshntsharma86@gmail.com>
2022-05-15 10:27:17 +0530

    Time: 117 ms (70.68%), Space: 62.8 MB (10.42%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -36,3 +36,17 @@ public:
         return sum;
     }
 };
+
+
+
+/*
+
+var deepestLeavesSum = function(root, depth = 0, sum = []) {
+  if (!root) { return; }
+  sum[depth] = (sum[depth] || 0) + root.val;
+  deepestLeavesSum(root.left, depth + 1, sum);
+  deepestLeavesSum(root.right, depth + 1, sum);
+  return  sum[sum.length - 1];
+};
+
+*/
