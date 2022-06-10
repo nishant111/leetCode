1b69422701328b9ba273c319bfa3ef1b41b03f3f
nishant111 <nshntsharma86@gmail.com>
2022-06-10 10:51:44 +0530

    Time: 382 ms (6.17%), Space: 9.5 MB (31.71%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -4,8 +4,8 @@ public:
     //u=> sequence undecided
     //i -> sequence increasing
     //d => sequence decreasing
-    
-    int dfs(vector<int> & rating, int curr, int pickSize, int lp, int seq, vector<vector<vector<int>>> &dp)
+    int dp[10001][4][3]={-1};
+    int dfs(vector<int> & rating, int curr, int pickSize, int lp, int seq)
     {
         if(pickSize == 3)
         {
@@ -25,21 +25,21 @@ public:
             {
                 int tmp = lp;
                 lp = rating[i];
-                ans += dfs(rating, i+1, pickSize+1, lp, seq, dp);
+                ans += dfs(rating, i+1, pickSize+1, lp, seq);
                 lp = tmp;
             }
             else if(lp != -1 && rating[i] > lp && (seq == 0 || seq == 1) )
             {
                 int tmp = lp;
                 lp = rating[i];
-                ans += dfs(rating, i+1, pickSize+1, lp, 1, dp);
+                ans += dfs(rating, i+1, pickSize+1, lp, 1);
                 lp = tmp;
             }
             else if(lp!=-1 && rating[i]<lp && (seq == 0 || seq == 2))
             {
                 int tmp = lp;
                 lp = rating[i];
-                ans  += dfs(rating, i+1, pickSize+1, lp, 2, dp);
+                ans  += dfs(rating, i+1, pickSize+1, lp, 2);
                 lp = tmp;
             }
         }
@@ -47,8 +47,9 @@ public:
     }
     int numTeams(vector<int>& rating) {
         int count = 0;
-        vector<vector<vector<int>>> dp(rating.size()+1, vector<vector<int>>(4, vector<int>(3, -1)));
-        return dfs(rating, 0, 0, -1, 0, dp);
+        //vector<vector<vector<int>>> dp(rating.size()+1, vector<vector<int>>(4, vector<int>(3, -1)));
+        memset (dp, -1, sizeof(dp));
+        return dfs(rating, 0, 0, -1, 0);
     }
 };
 
