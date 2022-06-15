9d53af974e28dd69db3022bfaf6fb1910c2f37b5
nishant111 <nshntsharma86@gmail.com>
2022-06-15 10:16:46 +0530

    Time: 81 ms (86.66%), Space: 21.3 MB (34.36%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,6 +1,84 @@
 class Solution {
 public:
     
+    int dfs(vector<string>& words, string curr, unordered_set<string> & st, unordered_map<string, int>& dp)
+    {
+        if(curr.size() == 1)
+            return dp[curr] = 1;
+        int ans = 1;
+        if(dp.find(curr)!=dp.end()) return dp[curr];
+        for(int i = curr.size()-1;i>=0;i--)
+        {
+            string toFind;
+            if(i == curr.size()-1)
+            {
+                toFind = curr.substr(0, i);
+            }
+            else if(i==0)
+            {
+                toFind = curr.substr(1, curr.size());
+            }
+            else
+            {
+                toFind = curr.substr(0, i) + curr.substr(i+1, curr.size()-1 - (i+1) + 1);
+            }
+            
+            if(st.find(toFind) != st.end())
+            {
+                ans = max(ans, 1+ dfs(words, toFind, st, dp));
+            }
+        }
+        return dp[curr] = ans;
+        
+    }
+    
+    int longestStrChain(vector<string>& words) {
+        unordered_set<string> m;
+        unordered_map<string, int> dp;
+        for(int i = 0;i<words.size();i++)
+        {
+            m.insert({words[i]});
+        }
+        int ans = INT_MIN;
+        for(int i = 0;i<words.size();i++)
+        {
+            ans = max(ans, dfs(words, words[i] , m, dp));
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    
     int dfs(unordered_set <string>& st, string currWord,
             unordered_map <string, int> &dp)
     {
@@ -34,4 +112,4 @@ public:
         }
         return ans;
     }
-};
+};*/
