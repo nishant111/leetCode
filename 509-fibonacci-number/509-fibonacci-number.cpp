110399136b82946309ca6e646ef8274ba6e3b28b
nishant111 <nshntsharma86@gmail.com>
2022-07-06 09:13:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int dp[31];
-    int fibinner(int n) {
+    int fibinner(int n, vector<int> &dp) {
         if(n == 0) return dp[n] = 0;
         if(n==1) return dp[n] = 1;
         if(dp[n]!=-1) return dp[n];
-        return dp[n] = (fib(n-2) + fib(n-1));
+        return dp[n] = (fibinner(n-2, dp) + fibinner(n-1, dp));
     }
     int fib(int n){
-        memset(dp, -1, sizeof(dp));
-        return fibinner(n);
+        vector<int> dp(n+1, -1);
+        return fibinner(n, dp);
     }
 };
