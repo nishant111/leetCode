1205758d729f68a74df9449850b4ae92f849e6b0
nishant111 <nshntsharma86@gmail.com>
2022-10-23 10:52:42 +0530

    Time: 27 ms (98.18%), Space: 21.3 MB (95.87%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
+    vector<int> findErrorNums1(vector<int>& nums) {
+        // O(n) time and O(n) space
         unordered_set <int> st;
         vector<int> ans;
         for(auto elem:nums)
@@ -14,6 +15,23 @@ public:
         {
             if(st.find(i) == st.end())
                 ans.push_back(i);
+        }
+        return ans;
+    }
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(nums[abs(nums[i])-1]<0)
+                ans.push_back(abs(nums[i]) );
+            else
+                nums[abs(nums[i])-1] *= -1;
+        }
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+                ans.push_back(i+1);
         }
         return ans;
     }
