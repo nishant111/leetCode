f965ab7c80993ff5b3a2d3973487078768ac2694
nishant111 <nshntsharma86@gmail.com>
2022-10-31 11:15:03 +0530

    Time: 32 ms (47.62%), Space: 17.5 MB (63.25%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -2,8 +2,28 @@ class Solution {
 public:
     /*
     Must go through below follow up answer:
-    https://leetcode.com/problems/toeplitz-matrix/discuss/516366/Python-Follow-Up-1-with-Explanation-and-Diagrams'=
+    https://leetcode.com/problems/toeplitz-matrix/discuss/516366/Python-Follow-Up-1-with-Explanation-and-Diagrams
+    Conclusion Algo 1:
+    Init Steps => create ExpectedDequeue, row=0 
+    1.load row in ExpectedDequeue(1234)
+    2. pop_back elem in ExpectedDequeue, push_front next row first elem, ExpectedDequeue becomes [5123]
+    3. compare with row++;
+    4. repeat step 2 & 3 until row<matrix.size();
+    
     https://leetcode.com/problems/toeplitz-matrix/discuss/271388/Java-Solution-for-Follow-Up-1-and-2
+    Conclusion:
+    Split matrix into submatrices with one row overlapping.
+    1234
+    3153
+    breaks into 
+    12  23  34
+    31  15  53
+    
+    if you dont overlap it will give wrong result(true)
+    12  34
+    31  53
+    
+    After breaking run Conclusion Algo 1 on all submatrices
     */
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int r=matrix.size()-1;
