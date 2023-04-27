0549f7ec8d244ea0050470d87d8aea1bfe28bcfe
nishant111 <nshntsharma86@gmail.com>
2023-04-27 09:37:32 +0530

    Time: 0 ms (100%), Space: 6.1 MB (9.91%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // floor of sqrt returns number of perfect squares upto n.
+        // floor(sqrt(20)) return number of prefect sq upto 20
+        return sqrt(n);
+    }
+};
