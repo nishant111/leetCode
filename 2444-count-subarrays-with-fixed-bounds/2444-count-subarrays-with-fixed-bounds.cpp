1d56377e4e1c20decbd7fbbccaf9f37122a13509
nishant111 <nshntsharma86@gmail.com>
2024-03-31 11:38:59 +0530

    Time: 90 ms (53.99%), Space: 82.5 MB (90.43%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        int badi = -1;
+        int  l = 0;
+        int r = 0;
         int lastMin = -1;
         int lastMax = -1;
+        int badi = -1;
         long long ans = 0;
-        for(int i = 0;i<nums.size();i++) { 
-            if(nums[i] == minK){
-                lastMin = i;
+        while(r<nums.size()){
+            if(nums[r] == minK){
+                lastMin = r;
             }
-            if(nums[i] == maxK){
-                lastMax = i;
+            if(nums[r] == maxK){
+                lastMax = r;
             }
-
-            if(nums[i] >= minK && nums[i] <= maxK)
-            {
-                ans += max(0,min(lastMin, lastMax)-badi);
+            if(nums[r]>=minK && nums[r]<=maxK){
+                // for ex 7, 1, 2, 3, 5, 2 for (minK = 2, maxk = 5)
+                // badi would be 0
+                // this helps include 1, 2, 3, 5 and 2, 3, 5 as 
+                // min(2,4)-0, helps us obtain 2 starting positions, when r = 4
+                ans += max(0, min(lastMin, lastMax)-badi);
             }
-            else {
-                badi = i;
+            else{
+                // this is something bad
+                // for ex 7, 1, 2, 3, 5, 2 for (minK = 2, maxk = 5)
+                // badi = 0
+                badi = r;
             }
-
+            r++;
         }
         return ans;
     }
