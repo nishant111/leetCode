13b6f18ac7c1c684ce14448aa6ebd108b8719529
nishant111 <nshntsharma86@gmail.com>
2024-06-15 11:01:09 +0530

    Time: 150 ms (43.17%), Space: 85.5 MB (63.02%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
+    int dfs(int k, int w, vector<int>& profits, vector<int>& capital, int curr){
+        if(k == 0)
+            return w;
+        if(curr == capital.size()){
+            return w;
+        }
+        int p = 0;
+        int np = 0;
+        if(w >= capital[curr]){
+            //try picking this up project
+            p = dfs(k-1, w+profits[curr], profits, capital, curr+1);
+        }
+        
+        //try skipping this project to maximize further
+        np = dfs(k, w, profits, capital, curr+1);
+        return max(p, np);
+    }
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        vector<pair<int,int>> proj;
-
-        for (int i = 0;i<profits.size();i++){
+        //return dfs(k, w, profits, capital, 0 );
+        
+        vector<pair<int, int>> proj;
+        for(int i = 0;i<profits.size();i++){
             proj.push_back({capital[i], profits[i]});
         }
-        priority_queue<int> pq;
         sort(proj.begin(), proj.end());
-        int ptr = 0;
-        for(int i = 0;i<k;i++){
-            while(ptr <proj.size() && proj[ptr].first<=w){
-                pq.push(proj[ptr].second);
-                ptr++;
+        priority_queue<int> q;
+        int pickCount = 0;
+        int iter = 0;
+        for(int i = 0;i<proj.size();i++ ){
+            while(iter<proj.size() && w >= proj[iter].first){
+                q.push(proj[iter].second);
+                iter++;
             }
-            if(pq.empty())
+            // if cant afford any project
+            if(q.empty())
                 break;
-            w += pq.top();
-            pq.pop();
+            w += q.top();
+            pickCount++;
+            q.pop();
+            if(pickCount == k)
+                break;
         }
         return w;
     }
