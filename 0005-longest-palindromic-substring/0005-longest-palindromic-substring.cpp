89e12c2536a692ef750b0e57c3c6f340c9f9f73a
nishant111 <nshntsharma86@gmail.com>
2024-07-31 11:40:46 +0530

    Time: 15 ms (73.19%), Space: 18.6 MB (53.37%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.size()==1) return s;
-        string ans = "";
-        //odd length
-        for(int i = 1;i<s.size();i++){
-            int l = i-1;
-            int r = i+1;
-            if(1>ans.size())
-                ans += s[i];
-            int size = 1;
-            while(l>=0 && r<s.size() && s[l]==s[r]){
-                size += 2;
-                if(size>ans.size())
-                    ans = s.substr(l,r-l+1);
-                l--;
-                r++;
+        if(s.size() == 1) return s;
+        // odd length expansion
+        string ans="";
+        for(int i = 0;i<s.size();i++){
+            int center = i;
+            int d = 0;
+            while(i-d>=0 && i+d<s.size() && s[i-d] == s[i+d]){
+                d++;
+            }
+            d--;
+            string p = s.substr(i-d, d+d+1);
+            if(p.size()>ans.size()){
+                ans = p;
             }
         }
         
-        //even length
+        // even length expansion
         for(int i = 0;i<s.size()-1;i++){
-            if(s[i]==s[i+1]){
-                int size = 2;
-                if(size>ans.size())
-                    ans = s.substr(i,2);
-                int l = i-1;
-                int r = i+1+1;
-                while(l>=0 && r<s.size() && s[l]==s[r]){
-                    size += 2;
-                    if(size>ans.size())
-                        ans = s.substr(l,r-l+1);
-                    l--;
-                    r++;
+            int center1 = i;
+            int center2 = i+1;
+            int d = 0;
+            if(s[center1] == s[center2]){
+                while(center1-d>=0 && center2+d<s.size() && s[center1-d] == s[center2+d]){
+                    d++;
                 }
+                d--;
+                string p = s.substr(center1-d, center2+d - (center1-d) + 1);
+                if(p.size()>ans.size())
+                    ans = p;
             }
         }
+        
         return ans;
     }
 };
