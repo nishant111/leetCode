350f0c07783777ab9be0d72bd222eef4086becf5
nishant111 <nshntsharma86@gmail.com>
2024-08-01 09:42:53 +0530

    Time: 4 ms (31.98%), Space: 7.9 MB (68.80%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
+    // edge case:
+    // leading zeros 
+    // number greater than 26
     int dfs(string &s, int curr, vector<int>& dp){
-        if(curr>=s.size()){
-            return 1;
-        }
+        
         if(s[curr]=='0')
             return 0;
-        if(dp[curr]!=-1) return dp[curr];
+        if(curr==s.size())
+            return 1;
+        if(dp[curr]!= -1){
+            return dp[curr];
+        }
+        int number = 0;
         int ways = 0;
-        
         ways += dfs(s, curr+1, dp);
-        int t = 0;
-        if(curr+1<s.size())
-            t = stoi(s.substr(curr, 2));
-            if(t>26)
-                t=0;
-        if(t!=0){
-            ways += dfs(s, curr+2, dp);
+        if(curr+1<s.size()){
+            number = (s[curr]-'0')*10 + (s[curr+1]-'0');
+            if(number>=1 && number <=26){
+                ways += dfs(s, curr+2, dp);
+            }
+            else{
+                return ways;
+            }
         }
         return dp[curr]=ways;
     }
     
+    
     int numDecodings(string s) {
-        unordered_map<int , char> mp;
         vector<int> dp(s.size()+1, -1);
         return dfs(s, 0, dp);
-        
     }
 };
