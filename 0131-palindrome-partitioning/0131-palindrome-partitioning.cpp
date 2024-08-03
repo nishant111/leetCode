2feff8ca1a97e95a96c9b97246e37b6c5956fade
nishant111 <nshntsharma86@gmail.com>
2024-08-03 12:25:00 +0530

    Time: 81 ms (73.28%), Space: 57.2 MB (66.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    bool isPalindrome(int start, int end, string &s){
-        while(start<=end){
-            if(s[start]!=s[end])
+    /* At each character we have 2 choices either to include it in the palindrome or start a new
+    palindrome, Hence 2^n choices and each time we take O(n) to check if its a plaindrome hence
+    O(2^n * n)*/
+    bool isP( string &s, int l, int r){
+        while(l<=r){
+            if(s[l]!=s[r]){
                 return false;
-            start++;
-            end--;
+            }
+            else{
+                r--;
+                l++;
+            }
         }
         return true;
     }
-    void dfs(string s, int curr,  vector<string> &currPali){
-        if(curr == s.size()){
-            ans.push_back(currPali);
+    
+    void dfs( string s, int idx, vector<string>& curr, vector<vector<string>>& ans){
+        
+        if(idx == s.size()){
+            // no more choices
+            ans.push_back(curr);
+            return;
         }
         
-        for(int i = curr;i<s.size();i++){
-            if(isPalindrome(curr, i, s)){
-                currPali.push_back(s.substr(curr, i-curr+1));
-                dfs(s, i+1, currPali);
-                currPali.pop_back();
+        for(int i = idx;i<s.size();i++){
+            if(isP(s, idx, i)){
+                curr.push_back(s.substr(idx, i-idx+1));
+                dfs(s, i+1, curr, ans);
+                curr.pop_back();
             }
         }
-        return;
+        
     }
     
     vector<vector<string>> partition(string s) {
-        vector<string> currPali;
-        dfs(s, 0, currPali);
+        vector<vector<string>> ans;
+        vector<string> currList;
+        dfs( s, 0, currList, ans);
         return ans;
     }
 };
