bf4c66f8e90cfc41cadc6fe93b32a1d06e86aca9
nishant111 <nshntsharma86@gmail.com>
2024-08-11 12:58:16 +0530

    Time: 11 ms (6.34%), Space: 13 MB (7.26%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
+    string getCurrElement( string& path, int& s){
+        string ans = "";
+        int start = s;
+        while( s<path.size() && path[s]!='/'){
+            ans += path[s++];
+        }
+        return path.substr(start, s - start);
+    }
+    
     string simplifyPath(string path) {
-        stack<string> st;
-        int l =0;
-        int r = 0;
-        
-        while(r<path.size()) {
-            string curr="";
-            if(path[r] == '/') {
-                r++;
-                while(r<path.size() && path[r] != '/'){
-                    r++;
-                }
-                curr = path.substr(l,r-l);
+        stack<string> st;    
+        for(int i = 0;i<path.size();){
+             while( i<path.size() && path[i]=='/'){
+                i++;
             }
-            cout << "curr is" << curr<<"\n";
-            if(curr == "/.." && !st.empty())
-            {
-                st.pop();
+            if(i == path.size()) break;
+            string curr = getCurrElement( path, i );
+            if(curr==".." ){
+                if( !st.empty())
+                    st.pop();
             }
-            if(curr != "/" && curr!= "/.." && curr!= "/.") {
+            else if(curr=="."){
+                continue;
+            }
+            else if(curr!="/"){
                 st.push(curr);
             }
-            l=r;
+           
         }
-        if(st.empty()) return "/";
         string ans = "";
-        while(!st.empty()) {
-            ans = st.top() + ans;
+        while(!st.empty()){
+            ans = '/'+st.top() + ans;
             st.pop();
+            
         }
-        return ans;
+        return ans.size()==0?"/":ans;
     }
 };
